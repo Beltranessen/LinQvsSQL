083bd300516c603c28204f2beec67f89342876eb
= <=>
2024-11-15 11:46:03 +0100

    Feature contacto creada

---
+++ LinQ con SQL/suscripcion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinQ_con_SQL
+{
+    public class suscripcion
+    {
+        public Guid Id { get; set; }
+        public string Version { get; set; }
+        public string Nombre { get; set; }
+
+    }
+}
